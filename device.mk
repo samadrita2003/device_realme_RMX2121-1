aac2ca678043d831a81e9f4c25be8ee6e7c1e1db
Nishant Kumar <www.rajsonu13@gmail.com>
2022-01-04 08:12:09 +0530

    Revert "RMX3031: Disable ART debug and optimize dexpreopt"
    This reverts commit 7c7e3c02c317ff8ef96f3620364eccf6ddfe9439.

--- device.mk
+++ device.mk
@@ -53,12 +53,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/permissions/privapp-permissions-camera-go.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-camera-go.xml
 
-# Dex/ART optimization
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-USE_DEX2OAT_DEBUG := false
-
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
